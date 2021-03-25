8d9ea0befb8b0b3968052e6ab4d6b34640679a7b
Prabhat Gupta <pgupta@collegeboard.org>
2021-03-25 15:11:12 +0530

    update

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -3,9 +3,11 @@ npm i
 npm i aws-cdk -g
 npm run cdk
 
+export UIBucketName=test-product-ui-submission
+echo $UIBKT
 cd Vue/app
 npm i
 
 npm run build
 
-aws s3 sync dist s3://product-ui-submission/
+aws s3 sync dist s3://$UIBucketName/
